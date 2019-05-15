842fe810b8fd10ff30becc80fa73aa7189065dc3
gwrxuk <gwrx2005@gmail.com>
2019-05-15 23:37:37 +0800

    Create fb.py

---
+++ fb.py
@@ -0,0 +1,244 @@
+from scrapy.crawler import CrawlerProcess
+import json
+import scrapy
+import logging
+from scrapy.loader import ItemLoader
+from scrapy.http import FormRequest
+from scrapy.exceptions import CloseSpider
+from datetime import datetime
+from fbcrawl.items import FbcrawlItem, parse_date, parse_date2
+
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('quoteresult.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+    
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    custom_settings = {
+        'FEED_EXPORT_FIELDS': ['source','shared_from','date','text', \
+                               'reactions','likes','ahah','love','wow', \
+                               'sigh','grrr','comments','post_id','url'],
+        'DUPEFILTER_CLASS' : 'scrapy.dupefilters.BaseDupeFilter',
+    }
+    start_urls = [
+        'https://mbasic.facebook.com/nytimes/',
+        'https://mbasic.facebook.com/nytimes/',
+    ]
+    custom_settings = {
+        'LOG_LEVEL': logging.WARNING,
+        'ITEM_PIPELINES': {'__main__.JsonWriterPipeline': 1}, # Used for pipeline 1
+        'FEED_FORMAT':'json',                                 # Used for pipeline 2
+        'FEED_URI': 'quoteresult.json'                        # Used for pipeline 2
+    }
+    def __init__(self, *args, **kwargs):
+        self.email = ""
+        self.password = ""
+        self.page = 'nytimes'
+        self.start_urls = ['https://mbasic.facebook.com']
+        self.lang = "en"
+        self.date = datetime(2019,5,18)
+        self.count = 0
+        self.k = datetime.now().year
+        self.max = int(10e5)
+        self.group = 0
+    
+    def parse(self, response):
+        '''
+        Handle login with provided credentials
+        '''
+        print(response)
+        return FormRequest.from_response(
+                response,
+                formxpath='//form[contains(@action, "login")]',
+                formdata={'email': self.email,'pass': self.password},
+                callback=self.parse_home
+                )
+  
+    def parse_home(self, response):
+        '''
+        This method has multiple purposes:
+        1) Handle failed logins due to facebook 'save-device' redirection
+        2) Set language interface, if not already provided
+        3) Navigate to given page 
+        '''
+        #handle 'save-device' redirection
+        if response.xpath("//div/a[contains(@href,'save-device')]"):
+            self.logger.info('Going through the "save-device" checkpoint')
+            return FormRequest.from_response(
+                response,
+                formdata={'name_action_selected': 'dont_save'},
+                callback=self.parse_home
+                )
+ 
+        #set language interface
+        if self.lang == '_':
+            if response.xpath("//input[@placeholder='Search Facebook']"):
+                self.logger.info('Language recognized: lang="en"')
+                self.lang = 'en'
+            elif response.xpath("//input[@placeholder='Buscar en Facebook']"):
+                self.logger.info('Language recognized: lang="es"')
+                self.lang = 'es'
+            elif response.xpath("//input[@placeholder='Rechercher sur Facebook']"):
+                self.logger.info('Language recognized: lang="fr"')
+                self.lang = 'fr'
+            elif response.xpath("//input[@placeholder='Cerca su Facebook']"):
+                self.logger.info('Language recognized: lang="it"')
+                self.lang = 'it'
+            elif response.xpath("//input[@placeholder='Pesquisa no Facebook']"):
+                self.logger.info('Language recognized: lang="pt"')
+                self.lang = 'pt'
+            else:
+                raise AttributeError('Language not recognized\n'
+                                     'Change your interface lang from facebook ' 
+                                     'and try again')
+                                                                 
+        #navigate to provided page
+        href = response.urljoin(self.page)
+        href="https://mbasic.facebook.com/爆料公社-162608724089621"
+        self.logger.info('Scraping facebook page {}'.format(href))
+        
+        return scrapy.Request(url=href,callback=self.parse_page,meta={'index':1})
+
+    def parse_page(self, response):
+        '''
+        Parse the given page selecting the posts.
+        Then ask recursively for another page.
+        '''
+#        #open page in browser for debug
+#        from scrapy.utils.response import open_in_browser
+#        open_in_browser(response)
+
+    
+        #select all posts
+        for post in response.xpath("//div[contains(@data-ft,'top_level_post_id')]"):     
+
+            many_features = post.xpath('./@data-ft').get()
+            date = []
+            date.append(many_features)
+
+            date = parse_date(date,{'lang':self.lang})
+            current_date = datetime.strptime(date,'%Y-%m-%d %H:%M:%S') if date is not None else date
+            print(post)
+            if current_date is None:
+                date_string = post.xpath('.//abbr/text()').get()
+                date = parse_date2([date_string],{'lang':self.lang})
+                current_date = datetime(date.year,date.month,date.day) if date is not None else date   
+                date = str(date)
+            print(current_date)   
+            #if 'date' argument is reached stop crawling
+            #if self.date > current_date:
+            #    raise CloseSpider('Reached date: {}'.format(self.date))
+            print("stop2")
+            new = ItemLoader(item=FbcrawlItem(),selector=post)
+            if abs(self.count) + 1 > self.max:
+                raise CloseSpider('Reached max num of post: {}. Crawling finished'.format(abs(self.count)))
+            self.logger.info('Parsing post n = {}, post_date = {}'.format(abs(self.count)+1,date))
+            new.add_xpath('comments', './div[2]/div[2]/a[1]/text()')     
+            new.add_value('date',date)
+            new.add_xpath('post_id','./@data-ft')
+            new.add_xpath('url', ".//a[contains(@href,'footer')]/@href")
+            #page_url #new.add_value('url',response.url)
+            
+            #returns full post-link in a list
+            post = post.xpath(".//a[contains(@href,'footer')]/@href").extract() 
+            temp_post = response.urljoin(post[0])
+        
+            self.count -= 1
+            yield scrapy.Request(temp_post, self.parse_post, priority = self.count, meta={'item':new})       
+
+        #load following page, try to click on "more"
+        #after few pages have been scraped, the "more" link might disappears 
+        #if not present look for the highest year not parsed yet
+        #click once on the year and go back to clicking "more"
+        
+        #new_page is different for groups
+        if self.group == 1:
+            new_page = response.xpath("//div[contains(@id,'stories_container')]/div[2]/a/@href").extract()      
+        else:
+            new_page = response.xpath("//div[2]/a[contains(@href,'timestart=') and not(contains(text(),'ent')) and not(contains(text(),number()))]/@href").extract()      
+            #this is why lang is needed                                            ^^^^^^^^^^^^^^^^^^^^^^^^^^               
+ 
+        if not new_page: 
+
+            self.logger.info('[!] "more" link not found, will look for a "year" link')
+            #self.k is the year link that we look for 
+            if 'flag' in response.meta and response.meta['flag'] == self.k and self.k >= self.year:                
+                xpath = "//div/a[contains(@href,'time') and contains(text(),'" + str(self.k) + "')]/@href"
+                new_page = response.xpath(xpath).extract()
+                if new_page:
+                    new_page = response.urljoin(new_page[0])
+                    self.k -= 1
+                    self.logger.info('Found a link for year "{}", new_page = {}'.format(self.k,new_page))
+                    yield scrapy.Request(new_page, callback=self.parse_page, meta={'flag':self.k})
+                else:
+                    while not new_page: #sometimes the years are skipped this handles small year gaps
+                        self.logger.info('Link not found for year {}, trying with previous year {}'.format(self.k,self.k-1))
+                        self.k -= 1
+                        if self.k < self.year:
+                            raise CloseSpider('Reached date: {}. Crawling finished'.format(self.date))
+                        xpath = "//div/a[contains(@href,'time') and contains(text(),'" + str(self.k) + "')]/@href"
+                        new_page = response.xpath(xpath).extract()
+                    self.logger.info('Found a link for year "{}", new_page = {}'.format(self.k,new_page))
+                    new_page = response.urljoin(new_page[0])
+                    self.k -= 1
+                    yield scrapy.Request(new_page, callback=self.parse_page, meta={'flag':self.k}) 
+            else:
+                self.logger.info('Crawling has finished with no errors!')
+        else:
+            new_page = response.urljoin(new_page[0])
+            if 'flag' in response.meta:
+                self.logger.info('Page scraped, clicking on "more"! new_page = {}'.format(new_page))
+                yield scrapy.Request(new_page, callback=self.parse_page, meta={'flag':response.meta['flag']})
+            else:
+                self.logger.info('First page scraped, clicking on "more"! new_page = {}'.format(new_page))
+                yield scrapy.Request(new_page, callback=self.parse_page, meta={'flag':self.k})
+                
+    def parse_post(self,response):
+    
+        new = ItemLoader(item=FbcrawlItem(),response=response,parent=response.meta['item'])
+        new.context['lang'] = self.lang           
+        new.add_xpath('source', "//td/div/h3/strong/a/text() | //span/strong/a/text() | //div/div/div/a[contains(@href,'post_id')]/strong/text()")
+        new.add_xpath('shared_from','//div[contains(@data-ft,"top_level_post_id") and contains(@data-ft,\'"isShare":1\')]/div/div[3]//strong/a/text()')
+     #   new.add_xpath('date','//div/div/abbr/text()')
+        new.add_xpath('text','//div[@data-ft]//p//text() | //div[@data-ft]/div[@class]/div[@class]/text()')
+     
+        #check reactions for old posts
+        check_reactions = response.xpath("//a[contains(@href,'reaction/profile')]/div/div/text()").get()
+        if not check_reactions:
+            print(new.load_item()["text"])       
+        else:
+            new.add_xpath('reactions',"//a[contains(@href,'reaction/profile')]/div/div/text()")              
+            reactions = response.xpath("//div[contains(@id,'sentence')]/a[contains(@href,'reaction/profile')]/@href")
+            reactions = response.urljoin(reactions[0].extract())
+            yield scrapy.Request(reactions, callback=self.parse_reactions, meta={'item':new})
+        
+    def parse_reactions(self,response):
+        new = ItemLoader(item=FbcrawlItem(),response=response, parent=response.meta['item'])
+        new.context['lang'] = self.lang           
+        new.add_xpath('likes',"//a[contains(@href,'reaction_type=1')]/span/text()")
+        new.add_xpath('ahah',"//a[contains(@href,'reaction_type=4')]/span/text()")
+        new.add_xpath('love',"//a[contains(@href,'reaction_type=2')]/span/text()")
+        new.add_xpath('wow',"//a[contains(@href,'reaction_type=3')]/span/text()")
+        new.add_xpath('sigh',"//a[contains(@href,'reaction_type=7')]/span/text()")
+        new.add_xpath('grrr',"//a[contains(@href,'reaction_type=8')]/span/text()")     
+        print(new.load_item())       
+
+            
+process = CrawlerProcess({
+    'USER_AGENT':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+})
+
+process.crawl(QuotesSpider)
+process.start()
